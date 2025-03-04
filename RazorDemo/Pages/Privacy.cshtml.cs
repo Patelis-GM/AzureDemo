593ddb1f8fd48e92aa6e169c29bb3e3b3771bba0
Giorgos Patelis <pgiorgosmarios@gmail.com>
2025-03-04 19:51:22 +0200

    Add project files.

---
+++ RazorDemo/Pages/Privacy.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorDemo.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+    public string EnvVariableMessage { get; private set; }
+
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        var envVar = Environment.GetEnvironmentVariable("MY_ENV_VAR_IN_AZURE");
+        EnvVariableMessage = string.IsNullOrEmpty(envVar) ? "Environment variable not set." : envVar;
+
+    }
+}
+
